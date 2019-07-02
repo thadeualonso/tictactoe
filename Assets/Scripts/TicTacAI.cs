abe2add6c475f9840fc0919443c6cd688dd61f94
Thadeu Alonso <thadeu.alonso@gmail.com>
2019-07-02 19:59:20 -0300

    Commit Inicial

---
+++ Assets/Scripts/TicTacAI.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/TicTacToe", fileName = "TicTacToeAI")]
+public class TicTacAI : ScriptableObject
+{
+    public struct Move
+    {
+        public int x;
+        public int y;
+
+        public Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public const int MAX_SCORE  = 10;
+    public const int MIN_SCORE  = -10;
+    public const int TIE_SCORE  = 0;
+    public const char PLAYER    = 'X';
+    public const char AI        = 'O';
+    public const char EMPTY     = '_';
+
+    public char PlayerLetter { get { return PLAYER; } }
+    public char AiLetter { get { return AI; } }
+
+    public Move FindBestMove(char[,] board, int height)
+    {
+        int bestScore = -1000;
+        Move bestMove = new Move(-1, -1);
+
+        for (int x = 0; x < board.Length / 3; x++)
+        {
+            for (int y = 0; y < board.Length / 3; y++)
+            {
+                if(board[x,y] == EMPTY)
+                {
+                    board[x, y] = PLAYER;
+                    int score = MinMax(0, board, false, height);
+                    board[x, y] = EMPTY;
+
+                    if(score > bestScore)
+                    {
+                        bestMove = new Move(x, y);
+                        bestScore = score;
+                    }
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    private int MinMax(int depth, char[,] board, bool isMax, int height)
+    {
+        int score = Evaluate(board);
+
+        //if (depth == height)
+          //  return score;
+
+        if (score == MAX_SCORE)
+            return score;
+
+        if (score == MIN_SCORE)
+            return score;
+
+        if (!IsMoveLefts(board))
+            return 0;
+
+        int bestScore = 0;
+
+        for (int x = 0; x < board.Length / 3; x++)
+        {
+            for (int y = 0; y < board.Length / 3; y++)
+            {
+                if(board[x,y] == EMPTY)
+                {
+                    board[x, y] = isMax ? PLAYER : AI;
+
+                    int value = MinMax(depth + 1, board, !isMax, height);
+
+                    bestScore = isMax ? Mathf.Max(bestScore, value) : Mathf.Min(bestScore, value);
+
+                    board[x, y] = EMPTY;
+                }
+            }
+        }
+
+        return bestScore;
+    }
+
+    private bool IsMoveLefts(char[,] board)
+    {
+        for (int x = 0; x < board.Length / 3; x++)
+        {
+            for (int y = 0; y < board.Length / 3; y++)
+            {
+                if (board[x, y] == EMPTY)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int Evaluate(char[,] board)
+    {
+        for (int row = 0; row < board.Length / 3; row++)
+        {
+            if(board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+            {
+                switch (board[row, 0])
+                {
+                    case PLAYER:
+                        return MAX_SCORE;
+                    case AI:
+                        return MIN_SCORE;
+                }
+            }
+        }
+
+        for (int col = 0; col < board.Length / 3; col++)
+        {
+            if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+            {
+                switch (board[0, col])
+                {
+                    case PLAYER:
+                        return MAX_SCORE;
+                    case AI:
+                        return MIN_SCORE;
+                }
+            }
+        }
+
+        if(board[0,0] == board[1,1] && board[1,1] == board[2,2])
+        {
+            switch (board[1, 1])
+            {
+                case PLAYER:
+                    return MAX_SCORE;
+                case AI:
+                    return MIN_SCORE;
+            }
+        }
+
+        if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
+        {
+            switch (board[1, 1])
+            {
+                case PLAYER:
+                    return MAX_SCORE;
+                case AI:
+                    return MIN_SCORE;
+            }
+        }
+
+        return TIE_SCORE;
+    }
+}
---
+++ Assets/Scripts/TicTacToeManager.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static TicTacAI;
+
+public class TicTacToeManager : MonoBehaviour
+{
+    public const string RESULT_PLAYER  = "Player wins!";
+    public const string RESULT_AI      = "AI wins!";
+    public const string RESULT_DRAW    = "Draw!";
+
+    [SerializeField] TicTacAI ai;
+    [SerializeField] List<Button> buttons;
+    [SerializeField] GameObject pnlFinish;
+
+    private char[,] board;
+    private Button[,] buttonArray;
+    private bool isGameFinished;
+    private bool drawLastRound;
+
+    public void StartGame()
+    {
+        isGameFinished = false;
+        SetupBoard();
+        SetupButtonArray();
+
+        if(drawLastRound)
+        {
+            AIMove();
+            drawLastRound = false;
+        }
+    }
+
+    private void SetupBoard()
+    {
+        board = new char[3, 3];
+
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                board[row, col] = '_';
+            }
+        }
+    }
+
+    private void SetupButtonArray()
+    {
+        buttonArray = new Button[3, 3];
+
+        buttonArray[0, 0] = buttons[0];
+        buttonArray[0, 1] = buttons[1];
+        buttonArray[0, 2] = buttons[2];
+        buttonArray[1, 0] = buttons[3];
+        buttonArray[1, 1] = buttons[4];
+        buttonArray[1, 2] = buttons[5];
+        buttonArray[2, 0] = buttons[6];
+        buttonArray[2, 1] = buttons[7];
+        buttonArray[2, 2] = buttons[8];
+
+        ClearBoard();
+    }
+
+    private void ClearBoard()
+    {
+        foreach (Button button in buttonArray)
+        {
+            button.interactable = true;
+            Text buttonText     = button.GetComponentInChildren<Text>();
+            buttonText.text     = "";
+        }
+    }
+
+    public void MakeMove(string boardIndex)
+    {
+        if (isGameFinished)
+            return;
+
+        int x = int.Parse(boardIndex.ToCharArray()[0].ToString());
+        int y = int.Parse(boardIndex.ToCharArray()[1].ToString());
+
+        board[x, y] = ai.PlayerLetter;
+        buttonArray[x, y].GetComponentInChildren<Text>().text = ai.PlayerLetter.ToString();
+
+        AIMove();
+
+        if (RowCrossed() == ai.PlayerLetter)
+            GameFinished(RESULT_PLAYER);
+        else if (RowCrossed() == ai.AiLetter)
+            GameFinished(RESULT_AI);
+
+        if (ColumnCrossed() == ai.PlayerLetter)
+            GameFinished(RESULT_PLAYER);
+        else if (ColumnCrossed() == ai.AiLetter)
+            GameFinished(RESULT_AI);
+
+        if (DiagonalCrossed() == ai.PlayerLetter)
+            GameFinished(RESULT_PLAYER);
+        else if (DiagonalCrossed() == ai.AiLetter)
+            GameFinished(RESULT_AI);
+    }
+
+    private void AIMove()
+    {
+        Move bestMove = ai.FindBestMove(board, 1);
+
+        if (bestMove.x == -1)
+        {
+            GameFinished(RESULT_DRAW);
+            return;
+        }
+
+        board[bestMove.x, bestMove.y] = ai.AiLetter;
+        Text buttonText = buttonArray[bestMove.x, bestMove.y].GetComponentInChildren<Text>();
+        buttonText.text = ai.AiLetter.ToString();
+        buttonArray[bestMove.x, bestMove.y].interactable = false;
+    }
+
+    private void GameFinished(string result)
+    {
+        if (result == RESULT_DRAW)
+            drawLastRound = true;
+
+        pnlFinish.GetComponent<CanvasGroup>().alpha = 1f;
+        pnlFinish.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        pnlFinish.GetComponentInChildren<TextMeshProUGUI>().text = result;
+        isGameFinished = true;
+    }
+
+    private char RowCrossed()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == '_')
+                continue;
+
+            if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                return board[i,0];
+        }
+
+        return '_';
+    }
+
+    private char ColumnCrossed()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[0, i] == '_')
+                continue;
+
+            if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                return board[0, i];
+        }
+
+        return '_';
+    }
+
+    private char DiagonalCrossed()
+    {
+        if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[1,1] != '_')
+            return board[0,0];
+
+        if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2] && board[1, 1] != '_')
+            return board[2,0];
+
+        return '_';
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
