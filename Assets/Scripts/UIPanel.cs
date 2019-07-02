1994a4100161d9dae180ef718661f56abffd1672
Thadeu Alonso <thadeu.alonso@gmail.com>
2019-07-02 20:37:36 -0300

    Script "UIPanel" adicionado

---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UIPanel : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void Show()
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void Hide()
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
