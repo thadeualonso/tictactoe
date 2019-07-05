141bcc155b7bdcc6242da0c32c8d7b53654e90a2
Thadeu Alonso <thadeu.alonso@gmail.com>
2019-07-05 14:24:27 -0300

    Build final

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -6,16 +6,7 @@ public class Player
 {
     [SerializeField] private string playerName;
     [SerializeField] private char letter;
-    [SerializeField] private bool isAI;
 
     public string PlayerName { get { return playerName; } private set { playerName = value; } }
     public char Letter { get { return letter; } private set { letter = value; } }
-    public bool IsAI { get { return isAI; } private set { isAI = value; } }
-
-    public Player(string playerName, char letter, bool isAI)
-    {
-        PlayerName = playerName;
-        Letter = letter;
-        IsAI = isAI;
-    }
 }
