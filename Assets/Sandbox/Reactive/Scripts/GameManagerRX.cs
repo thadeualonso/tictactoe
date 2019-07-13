43eb5bbd2caa20bda749f6c6b35073e532cf4c59
Thadeu Alonso <thadeu.alonso@gmail.com>
2019-07-13 19:16:47 -0300

    Importação do Asset UniRX

---
+++ Assets/Sandbox/Reactive/Scripts/GameManagerRX.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Operators;
+using UnityEngine.UI;
+using UniRx.Triggers;
+
+namespace TicTacToe.Reactive
+{
+    public class GameManagerRX : MonoBehaviour
+    {
+        [SerializeField] Button button00;
+
+        private void Start()
+        {
+            var clickStream = button00.OnClickAsObservable().Select(_ => new Move(0, 0));
+            clickStream.Subscribe(x => OnClick(x));
+        }
+
+        void OnClick(Move move)
+        {
+            Debug.Log("Move: " + move.X + " " + move.Y);
+        }
+    }
+}
