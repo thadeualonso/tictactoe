7db4cd2c5462633815377297363a5bda47e39187
Thadeu Alonso <thadeu.alonso@gmail.com>
2019-09-05 00:39:22 -0300

    Importação da dll NSubstitute e inicio dos testes unitarios

---
+++ Assets/Scripts/Tests/GameManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+
+namespace Tests
+{
+    public class GameManagerTests
+    {
+        [Test]
+        public void check_if_start_game_is_called()
+        {
+            GameManager gameManager = Substitute.For<GameManager>();
+        }
+    }
+}
